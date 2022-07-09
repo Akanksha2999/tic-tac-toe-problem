76ee4aa8f99bc14a81af2fbc24599e4d86031a45
Akanksha2999 <akankshajadhav2999@gmail.com>
2022-07-09 09:01:49 +0530

    ADDED: User can make choice between X or 0 to play the game

--- com/bridgelabz/TicTacToeGame.java
+++ com/bridgelabz/TicTacToeGame.java
@@ -1,19 +1,33 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
     public static char[] gameBoard = new char[10];
+    public static char playerTurn;
+    public static char computerTurn;
+    public static char user = '1';
+    public static Scanner scanner = new Scanner(System.in);
 
-    //Method to create the board
+    //Main method
+    public static void main(String[] args) {
+        System.out.println("Welcome to Tic Tac Toe game");
+        showBoard();
+        player();
+
+    }
+
+    //Method to print the game board
     public static void showBoard() {
+
         System.out.println(gameBoard[0] + " | " + gameBoard[1] + " | " + gameBoard[2]);
         System.out.println(gameBoard[3] + " | " + gameBoard[4] + " | " + gameBoard[5]);
         System.out.println(gameBoard[6] + " | " + gameBoard[7] + " | " + gameBoard[8]);
     }
 
-    //Main method
-    public static void main(String[] args) {
-        System.out.println("Welcome to tic tac toe program");
-        System.out.println("The game board with 9 spaces is as follows");
-        showBoard();
+    //Method for user to choose among X or 0
+    public static char player() {
+        System.out.println("Enter your choice -> (X or O) only");
+        return scanner.next().toUpperCase().charAt(0);
     }
 }
