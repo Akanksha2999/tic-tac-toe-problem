23e09f5168e287e219801f0c5c91df9222dc207c
Akanksha2999 <akankshajadhav2999@gmail.com>
2022-07-09 09:27:43 +0530

    ADDED: User makes choice between location 1 to 9 in board

--- com/bridgelabz/TicTacToeGame.java
+++ com/bridgelabz/TicTacToeGame.java
@@ -4,30 +4,26 @@ import java.util.Scanner;
 
 public class TicTacToeGame {
     public static char[] gameBoard = new char[10];
-    public static char playerTurn;
-    public static char computerTurn;
     public static char user = '1';
     public static char userTurn;
     public static int userPos;
-    public static int computerPos;
-    public static char exitCode = '0';
-    public static int turnCount;
     public static Scanner scanner = new Scanner(System.in);
 
     //Main method
     public static void main(String[] args) {
-        System.out.println("Welcome to Tic Tac Toe game");
+        System.out.println("Welcome to Tic Tac Toe program");
         showBoard();
         player();
         ticTacToe();
+        moveLocation();
     }
 
     //Method to print the game board
     public static void showBoard() {
 
-        System.out.println(gameBoard[0] + " | " + gameBoard[1] + " | " + gameBoard[2]);
-        System.out.println(gameBoard[3] + " | " + gameBoard[4] + " | " + gameBoard[5]);
-        System.out.println(gameBoard[6] + " | " + gameBoard[7] + " | " + gameBoard[8]);
+        System.out.println(gameBoard[1] + " | " + gameBoard[2] + " | " + gameBoard[3]);
+        System.out.println(gameBoard[4] + " | " + gameBoard[5] + " | " + gameBoard[6]);
+        System.out.println(gameBoard[7] + " | " + gameBoard[8] + " | " + gameBoard[9]);
     }
 
     //Method for user to choose among X or 0
@@ -42,6 +38,70 @@ public class TicTacToeGame {
 
             gameBoard[i] = ' ';
         }
-    }
+    }//User choose location in board from 1 to 9
+    public static void moveLocation() {
+        if (user == '1') {
+            System.out.print("\nSelect the position in board from range [1 - 9] : ");
+            userPos = scanner.nextInt();
 
+            switch (userPos) {
+                case 1:
+                    if (gameBoard[1] == ' ') {
+                        gameBoard[1] = userTurn;
+                    }
+                    break;
+
+                case 2:
+                    if (gameBoard[2] == ' ') {
+                        gameBoard[2] = userTurn;
+                    }
+                    break;
+
+                case 3:
+                    if (gameBoard[3] == ' ') {
+                        gameBoard[3] = userTurn;
+                    }
+                    break;
+
+                case 4:
+                    if (gameBoard[4] == ' ') {
+                        gameBoard[4] = userTurn;
+                    }
+                    break;
+
+                case 5:
+                    if (gameBoard[5] == ' ') {
+                        gameBoard[5] = userTurn;
+                    }
+                    break;
+
+                case 6:
+                    if (gameBoard[6] == ' ') {
+                        gameBoard[6] = userTurn;
+                    }
+                    break;
+
+                case 7:
+                    if (gameBoard[7] == ' ') {
+                        gameBoard[7] = userTurn;
+                    }
+                    break;
+
+                case 8:
+                    if (gameBoard[8] == ' ') {
+                        gameBoard[8] = userTurn;
+                    }
+                    break;
+
+                case 9:
+                    if (gameBoard[9] == ' ') {
+                        gameBoard[9] = userTurn;
+                    }
+                    break;
+
+                default:
+                    System.out.println("Invalid Position");
+            }
+        }
+    }
 }
